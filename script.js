01a03799c71a8b089cd2f957414ac755cba9a330
pk91990 <96700190+pk91990@users.noreply.github.com>
2025-08-12 01:17:09 +0530

    animation

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-gsap.registerPlugin(SplitText);
+ // Register plugin
+  gsap.registerPlugin(SplitText);
 
-  // Split text into words and characters
-  let split = SplitText.create(".split", { type: "words, chars" });
+  // Split text into characters
+  let split = SplitText.create(".name", { type: "chars" });
 
-  // Animate characters one-by-one
+  // Animate characters
   gsap.from(split.chars, {
-    duration: 1,
-    y: 100,
-    autoAlpha: 0,
-    stagger: 0.05
+    duration: 2,
+    y: 80,            // move from below
+    rotationX: 90,    // slight 3D flip
+    opacity: 0,       // fade-in
+    ease: "back.out(1.7)",
+    stagger: 0.1     // delay between letters
   });
